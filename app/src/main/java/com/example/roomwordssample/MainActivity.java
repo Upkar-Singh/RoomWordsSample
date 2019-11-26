81b9ce7a2acf91806d4b1b24fc0a47f407bd9202
upkar-singh <uupkarssingh@gmail.com>
2019-11-26 14:15:27 -0500

    not done

--- app/src/main/java/com/example/roomwordssample/MainActivity.java
+++ app/src/main/java/com/example/roomwordssample/MainActivity.java
@@ -22,6 +22,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private WordViewModel mWordViewModel;
+
     public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
@@ -31,12 +32,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Setup the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final WordListAdapter adapter = new WordListAdapter(this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
 
+        // Setup the WordViewModel
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+        // Get all the words from the database
+        // and associate them to the adapter
         mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
             @Override
             public void onChanged(@Nullable final List<Word> words) {
@@ -44,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setWords(words);
             }
         });
+
+        // Floating action button setup
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 new ItemTouchHelper.SimpleCallback(0,
                         ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
                     @Override
+                    // We are not implementing onMove() in this app
                     public boolean onMove(RecyclerView recyclerView,
                                           RecyclerView.ViewHolder viewHolder,
                                           RecyclerView.ViewHolder target) {
@@ -66,18 +74,20 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onSwiped(RecyclerView.ViewHolder viewHolder,
-                                         int direction) {
+                    // When the use swipes a word,
+                    // delete that word from the database.
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
                         int position = viewHolder.getAdapterPosition();
                         Word myWord = adapter.getWordAtPosition(position);
-                        Toast.makeText(MainActivity.this, "Deleting " +
-                                myWord.getWord(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this,
+                                getString(R.string.delete_word_preamble) + " " +
+                                        myWord.getWord(), Toast.LENGTH_LONG).show();
 
                         // Delete the word
                         mWordViewModel.deleteWord(myWord);
                     }
                 });
-
+        // Attach the item touch helper to the recycler view
         helper.attachToRecyclerView(recyclerView);
     }
 
@@ -88,34 +98,45 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // The options menu has a single item "Clear all data now"
+    // that deletes all the entries in the database
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
         if (id == R.id.clear_data) {
             // Add a toast just for confirmation
-            Toast.makeText(this, "Clearing the data...",
-                    Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.clear_data_toast_text, Toast.LENGTH_LONG).show();
 
             // Delete the existing data
             mWordViewModel.deleteAll();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+    /** When the user enters a new word in the NewWordActivity,
+     * that activity returns the result to this activity.
+     * If the user entered a new word, save it in the database.
+     * @param requestCode -- ID for the request
+     * @param resultCode -- indicates success or failure
+     * @param data -- The Intent sent back from the NewWordActivity,
+     *             which includes the word that the user entered
+     */
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
             Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            // Save the data
             mWordViewModel.insert(word);
         } else {
             Toast.makeText(
-                    getApplicationContext(),
-                    R.string.empty_not_saved,
-                    Toast.LENGTH_LONG).show();
+                    this, R.string.empty_not_saved, Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -50,7 +50,8 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
         private final WordDao mDao;
-        String[] words = {"dolphin", "crocodile", "cobra"};
+        private static String [] words = {"dolphin", "crocodile", "cobra", "elephant", "goldfish",
+                "tiger", "snake"};
 
         PopulateDbAsync(WordRoomDatabase db) {
             mDao = db.wordDao();
